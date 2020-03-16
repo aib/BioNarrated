976403ce425671ac5be2fff39b955d0cef81fd3d
aib <aibok42@gmail.com>
2020-03-16 17:36:44 +0300

    Add a retry-with-delay mechanism to GrovePi bus transfers

--- grovepi.py
+++ grovepi.py
@@ -6,6 +6,7 @@ class GrovePi:
 	def __init__(self, i2c_bus=1, i2c_addr=4):
 		self.bus = smbus.SMBus(i2c_bus)
 		self.i2c_addr = i2c_addr
+		self.retries = [0]
 
 	def digitalRead(self, pin):
 		self._write(1, [pin, 0, 0])
@@ -22,15 +23,25 @@ class GrovePi:
 		return 256 * data[1] + data[2]
 
 	def _write(self, cmd, params):
-		self.bus.write_i2c_block_data(self.i2c_addr, cmd, params)
-		time.sleep(.001)
+		self._retry(lambda: self.bus.write_i2c_block_data(self.i2c_addr, cmd, params))
 
 	def _read(self, cmd, length):
-		data = self.bus.read_i2c_block_data(self.i2c_addr, cmd, length)
-		time.sleep(.001)
+		data = self._retry(lambda: self.bus.read_i2c_block_data(self.i2c_addr, cmd, length))
 		return data
 
 	def _read1(self):
-		data = self.bus.read_byte(self.i2c_addr)
-		time.sleep(.001)
+		data = self._retry(lambda: self.bus.read_byte(self.i2c_addr))
 		return data
+
+	def _retry(self, cmd):
+		retries = iter(self.retries)
+		while True:
+			try:
+				return cmd()
+			except OSError as e:
+				delay = next(retries, None)
+				if delay is None:
+					raise
+				else:
+					time.sleep(delay)
+					continue
